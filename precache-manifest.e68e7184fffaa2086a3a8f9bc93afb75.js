472dddf5b6a06d8d5247db383a4a53dd42bc113c
a-malacarne <a-malacarne@users.noreply.github.com>
2021-06-26 21:50:00 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.e68e7184fffaa2086a3a8f9bc93afb75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f98c9092a67058f50eb092af05519d5",
+    "url": "https://a-malacarne.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "96de29cbef299dadaab6",
+    "url": "https://a-malacarne.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c1f99d82d0d8cd6b4ff2",
+    "url": "https://a-malacarne.github.io/task-management-frontend/static/js/2.83edaeb8.chunk.js"
+  },
+  {
+    "revision": "96de29cbef299dadaab6",
+    "url": "https://a-malacarne.github.io/task-management-frontend/static/js/main.1224b47a.chunk.js"
+  },
+  {
+    "revision": "1aca2a22ae9f5cb2ca6d",
+    "url": "https://a-malacarne.github.io/task-management-frontend/static/js/runtime~main.6a71bf43.js"
+  }
+]);
